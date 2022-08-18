f214231655ed0537d3d1f5bdd947802cc4bcc014
Clinton Mutinda <mutinda1993@gmail.com>
2022-08-18 08:01:34 +0300

    Added form arrays and form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
 
+//import { AngularFireModule } from '@angular/fire';
+import { initializeApp } from "firebase/app";
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { AngularFireStorageModule } from '@angular/fire/storage';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBV72o6a1KL2YN7tLN_gLs7ENG7cobcH08",
+  authDomain: "demos-d499a.firebaseapp.com",
+  projectId: "demos-d499a",
+  storageBucket: "demos-d499a.appspot.com",
+  messagingSenderId: "782370679110",
+  appId: "1:782370679110:web:2d3caa9177ba570e3b5d47",
+  measurementId: "G-3QZ4F9Q451"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
 
   myForm: FormGroup;
+  myForm2: FormGroup;
+  myForm3: FormGroup;
 
   constructor(
     private fb: FormBuilder
@@ -16,16 +18,82 @@ export class ReactiveFormComponent implements OnInit {
 
   ngOnInit(): void {
 
+    const phone  = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    })
+
     this.myForm = this.fb.group({
       email:'',
+      homePhone: phone,
+      cellPhone: phone,
       message: 'Default message'
     })
 
+    this.myForm2 = this.fb.group({
+      email:'',
+      phones: this.fb.array([])
+    })
+
+    this.myForm3 = this.fb.group({
+
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+
+      password: ['', [
+        Validators.required,
+        Validators.pattern('/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')
+      ]],
+
+      age:[null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.min(18),
+        Validators.max(65)
+      ]]
+    });
+
+
     //the valueChanges method returns an observable
-    //when we subscribe to the observable, we emit the value of the form
-    //everytime it changes
+    //when we subscribe to the observable, 
+    //we emit the value of the for everytime it changes
     this.myForm.valueChanges.subscribe(console.log);
 
   }
 
+  get phoneForms() {
+    return this.myForm2.get('phones') as FormArray
+  }
+  
+  addPhone(){
+
+    const phone  = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    })
+
+    this.phoneForms.push(phone);
+
+  }
+
+  deletePhone(i){
+    this.phoneForms.removeAt(i);
+  }
+
+  get email(){
+    return this.myForm3.get('email');
+  }
+
+  get password() {
+    return this.myForm3.get('password');
+  }
+
+  get age(){
+    return this.myForm3.get('age');
+  }
+  
 }
